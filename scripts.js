ef739ab2fd2fbd2e87623bf0ce500b51404e2e64
Julia Blahut <julia@Julias-MBP.localdomain>
2024-05-21 09:21:02 -0400

    redesigned home page

--- scripts.js
+++ scripts.js
@@ -8,28 +8,26 @@ linkedInButton.addEventListener("mouseout", linkedInLeave);
 
 function emailHover() {
     emailButton.style.textDecoration = "none";
-    emailButton.style.color = "#FEFEFA";
-    emailButton.style.backgroundColor = "#455b55";
-    emailButton.style.borderRadius = "50px";
+    emailButton.style.color = "white";
+    emailButton.style.backgroundColor = "#C98686";
     emailButton.style.padding = "0px 5px 0 5px";
 }
 
 function linkedInHover() {
     linkedInButton.style.textDecoration = "none";
-    linkedInButton.style.color = "#FEFEFA";
-    linkedInButton.style.backgroundColor = "#455b55";
-    linkedInButton.style.borderRadius = "50px";
+    linkedInButton.style.color = "white";
+    linkedInButton.style.backgroundColor = "#C98686";
     linkedInButton.style.padding = "0px 5px 0 5px";
 }
 
 function emailLeave() {
     emailButton.style.textDecoration = "none";
-    emailButton.style.color = "#455b55";
-    emailButton.style.backgroundColor = "#FEFEFA";
+    emailButton.style.color = "white";
+    emailButton.style.backgroundColor = "#4B644A";
 }
 
 function linkedInLeave() {
     linkedInButton.style.textDecoration = "none";
-    linkedInButton.style.color = "#455b55";
-    linkedInButton.style.backgroundColor = "#FEFEFA";
+    linkedInButton.style.color = "white";
+    linkedInButton.style.backgroundColor = "#4B644A";
 }
