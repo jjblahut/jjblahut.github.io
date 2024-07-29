671814d208ffd873ddf8e3a041429a6c0200573c
Julia Blahut <julia@Julias-MacBook-Pro.local>
2024-07-29 17:32:54 -0400

    Nav bar and welcome div

--- scripts.js
+++ scripts.js
@@ -31,3 +31,12 @@ function linkedInLeave() {
     linkedInButton.style.color = "white";
     linkedInButton.style.backgroundColor = "#4B644A";
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
